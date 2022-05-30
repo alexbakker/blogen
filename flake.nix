6b98804ae730e0fe74ed4806fb7e9be662abd380
Alexander Bakker <ab@alexbakker.me>
2022-05-30 18:14:10 +0200

    Remove version from flake.nix

--- flake.nix
+++ flake.nix
@@ -9,8 +9,7 @@
         pkgs = import nixpkgs { inherit system; };
       in {
         defaultPackage = with pkgs; buildGoModule {
-          pname = "blogen";
-          version = "0.0.0";
+          name = "blogen";
           src = ./.;
 
           vendorSha256 = "sha256-yJ+llGnGJcN1CMmVYs86qK5DKlyYd5JGDWare7Aeu4A=";
